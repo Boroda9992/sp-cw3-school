8c4241354c7e2955b3175b2dbc20af6b6fe5808a
Boroda9992 <fedko.ak@gmail.com>
2025-02-05 00:31:46 +0300

    3.5 Обработка файлов. - Созданы модель, репозиторий и сервис Avatar - В контроллере создан эндпоинт для загрузки аватара uploadAvatar. - В контроллере созданы эндпоинты для получения аватара из ФС и БД: downloadAvatar и downloadAvatarPreview соответственно.

---
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -0,0 +1,11 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
+
+import java.io.IOException;
+
+public interface AvatarService {
+    void uploadAvatar(MultipartFile avatar, long avatarId) throws IOException;
+    Avatar findAvatar(long studentId);
+}
